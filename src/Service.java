612a29e52a328ce205de37999a76fba91fc1616d
Robbag96 <robbag077@gmail.com>
2022-01-12 14:22:28 +0400

    Write and compare by name

--- src/Service.java
+++ src/Service.java
@@ -1,11 +1,28 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringJoiner;
 
 public class Service {
 
     public static String[] read(String path) throws IOException {
 
         return  Files.readAllLines(Paths.get(path)).toArray(new String[0]);
+    }
+    static void write(String path, String text) throws IOException {
+        Files.write(Paths.get(path),text.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+    }
+
+    static String sortByName(List<Human> humans){
+        Collections.sort(humans,new ComparatorName());
+        StringJoiner sj = new StringJoiner(" ");
+        for (Human hm:humans){
+        sj.add(hm.toString());
+        }
+        return sj.toString();
     }
 }
